1e7af27b461c58f27ef6afda70dfd84ada7d8237
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2017-03-11 14:59:06 +0900

    エリア、分類ごとに集計して表示

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,15 @@
 class Portfolio < ApplicationRecord
   extend Enumerize
 
-  enumerize :area, in: %i(japan emerging developed)
-  enumerize :category, in: %i(stock bond reit)
+  enumerize :area, in: %i(japan emerging developed), scope: true
+  enumerize :category, in: %i(stock bond reit), scope: true
+
+  def self.area_text(value)
+    # TODO: Enumerizeで何かあるのでは？
+    Portfolio.area.options.find{|k,v| v == value }[0]
+  end
+
+  def self.category_text(value)
+    Portfolio.category.options.find{|k,v| v == value }[0]
+  end
 end
